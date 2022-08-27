07cdc564ff7abb2283cc7755eb2fe9cadc1d481a
Shii_039 <enshikuku39@gmail.com>
2022-08-27 12:43:17 +0300

    first commit

--- main.js
+++ main.js
@@ -12,3 +12,24 @@ questions.forEach(question => {
         }
     })
 })
+
+let load = document.querySelector('.loader')
+window.addEventListener("load", function(){
+    load.style.display = 'none'
+})
+mybutton = document.querySelector('#scrollTopBtn')
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
